164213879f8f0945b09dab712b3956364afe992a
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2022-04-10 19:40:11 +0200

    feat: Insert "World" if the passed name is emtpy

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Robin")
-	want := "Hello, Robin"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMesssage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Robin")
+		want := "Hello, Robin"
+		assertCorrectMesssage(t, got, want)
+
+	})
+	t.Run("say 'Hello World' when a empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMesssage(t, got, want)
+
+	})
+
 }
