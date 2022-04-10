36bfc3957945d9a5c7c5f69ad52d089d72c8837e
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2022-04-10 20:14:34 +0200

    feat: Add support for multiple languages

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -3,14 +3,29 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello,"
+const spanishHelloPrefix = "Hola,"
+const germanHelloPrefix = "Hallo,"
 
-func Hello(name string) string {
+func Hello(name, language string) string {
+	prefix := greetingPrefix(language)
 	if len(name) == 0 {
 		name = "World"
 	}
-	return fmt.Sprintf("%s %s", englishHelloPrefix, name)
+	return fmt.Sprintf("%s %s", prefix, name)
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "German":
+		prefix = germanHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Robin"))
+	fmt.Println(Hello("Robin", "English"))
 }
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -11,16 +11,30 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Robin")
+		got := Hello("Robin", "English")
 		want := "Hello, Robin"
 		assertCorrectMesssage(t, got, want)
 
 	})
-	t.Run("say 'Hello World' when a empty string is passed", func(t *testing.T) {
-		got := Hello("")
+	t.Run("saying'Hello World' when a empty string is passed", func(t *testing.T) {
+		got := Hello("", "English")
 		want := "Hello, World"
 		assertCorrectMesssage(t, got, want)
 
 	})
-
+	t.Run("saying the greeting in Spanish", func(t *testing.T) {
+		got := Hello("Robin", "Spanish")
+		want := "Hola, Robin"
+		assertCorrectMesssage(t, got, want)
+	})
+	t.Run("saying the greeting in German", func(t *testing.T) {
+		got := Hello("Robin", "German")
+		want := "Hallo, Robin"
+		assertCorrectMesssage(t, got, want)
+	})
+	t.Run("Using English as default when a unknown langauge was passed", func(t *testing.T) {
+		got := Hello("Robin", "Italian")
+		want := "Hello, Robin"
+		assertCorrectMesssage(t, got, want)
+	})
 }
