2030e054673434df77f14891c5a041883e911c3c
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2022-04-22 11:34:22 +0200

    Add handling of unkonwn words

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,15 @@
 package main
 
-func Search(d map[string]string, key string) string {
-	return d[key]
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for.")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,13 +3,30 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	key := "test"
 	value := "This is just a test"
-	dictionary := map[string]string{key: value}
-	got := Search(dictionary, key)
-	want := value
+	dictionary := Dictionary{"test": value}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		assertStrings(t, got, value)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("bla")
+		assertError(t, err, ErrNotFound)
+	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, key)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
