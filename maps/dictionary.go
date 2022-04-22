c614fe52b1734d7f765c841e8ca1554cd05147f2
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2022-04-22 10:38:04 +0200

    Add search function

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,1 +1,5 @@
 package main
+
+func Search(d map[string]string, key string) string {
+	return d[key]
+}
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,1 +1,15 @@
 package main
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	key := "test"
+	value := "This is just a test"
+	dictionary := map[string]string{key: value}
+	got := Search(dictionary, key)
+	want := value
+
+	if got != want {
+		t.Errorf("got %q want %q given %q", got, want, key)
+	}
+}
